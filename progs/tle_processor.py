de23b1479f2febb095495c7785f18ee5e579021e
Arash <vermissa0ss@aol.com>
2022-11-10 23:43:31 -0600

    Add a parser to read multiple TLE files

---
+++ progs/tle_processor.py
@@ -0,0 +1,18 @@
+class process_multiple_TLEs:
+
+
+    def load_TLE(self, TLE_file):
+        # Input: TLE file location
+        # Output: Dictionary of multiple satellites with 2line elements as values
+
+        print('Reading TLE file ', TLE_file)
+        txtFile = open(TLE_file).read() #read file
+        loaded_lines = txtFile.split("\n") #split at every new line
+        
+        # basic TLE format for each satellite consists of three lines.
+        # Line1 = GPS satellite code ID or name known as PRN
+        # Line 2 = line number 1
+        # Line 3 = line number 2 
+        #Create a dictionary made of only PRN code as key ([-7:-1]). and keep the next two lines as its values
+        dic_satellites_twoLine = { loaded_lines[i][-7:-1] : [loaded_lines[i+1], loaded_lines[i+2]] for i in range(0, len(loaded_lines),3 ) }
+        return dic_satellites_twoLine
